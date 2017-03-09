6ccb7374686719bb2a378208a5a02315730dd7d9
liushengyuan <321019932@qq.com>
2017-03-09 17:44:26 +0800

    订单表实体类

---
+++ src/main/java/com/springdemo/po/Order.java
@@ -0,0 +1,61 @@
+package com.springdemo.po;
+
+public class Order {
+	private int id;
+	private int cat_id;
+	private String code;
+	private int member_id;
+	private long time;
+	private int status;//0 未支付  1  已经支付
+	private String address;
+	private int phone;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCat_id() {
+		return cat_id;
+	}
+	public void setCat_id(int cat_id) {
+		this.cat_id = cat_id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public int getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(int member_id) {
+		this.member_id = member_id;
+	}
+	public long getTime() {
+		return time;
+	}
+	public void setTime(long time) {
+		this.time = time;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	
+}
