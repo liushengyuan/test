d00cfc752ced69f05509316f29c68a15d5d31b7a
liushengyuan <321019932@qq.com>
2017-03-09 10:28:25 +0800

    添加购物车实体类

---
+++ src/main/java/com/springdemo/po/Cart.java
@@ -0,0 +1,47 @@
+package com.springdemo.po;
+
+public class Cart {
+	private int id;
+	private int goods_id;
+	private String goods_name;
+	private String goods_image1;
+	private double goods_price;
+	private int is_select;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getGoods_id() {
+		return goods_id;
+	}
+	public void setGoods_id(int goods_id) {
+		this.goods_id = goods_id;
+	}
+	public String getGoods_name() {
+		return goods_name;
+	}
+	public void setGoods_name(String goods_name) {
+		this.goods_name = goods_name;
+	}
+	public String getGoods_image1() {
+		return goods_image1;
+	}
+	public void setGoods_image1(String goods_image1) {
+		this.goods_image1 = goods_image1;
+	}
+	public double getGoods_price() {
+		return goods_price;
+	}
+	public void setGoods_price(double goods_price) {
+		this.goods_price = goods_price;
+	}
+	public int getIs_select() {
+		return is_select;
+	}
+	public void setIs_select(int is_select) {
+		this.is_select = is_select;
+	}
+	
+}
